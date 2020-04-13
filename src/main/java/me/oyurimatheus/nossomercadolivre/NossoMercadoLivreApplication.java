246eccdbafd71ae5c92cdfaa6fa91ec9d594b067
Yuri Matheus <yurimatheusb@hotmail.com>
2020-04-13 19:05:02 -0300

    add spring security dependency

--- src/main/java/me/oyurimatheus/nossomercadolivre/NossoMercadoLivreApplication.java
+++ src/main/java/me/oyurimatheus/nossomercadolivre/NossoMercadoLivreApplication.java
@@ -2,8 +2,9 @@ package me.oyurimatheus.nossomercadolivre;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class NossoMercadoLivreApplication {
 
 	public static void main(String[] args) {
